4b538ca0804a190c10364b5c2ff18755e68cf81a
Pablo Philipps <pabloignaciophilipps@gmail.com>
2020-07-04 17:20:35 -0500

    Initial Commit

---
+++ pablq-app/pablq_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pablq_appApp.swift
+//  pablq-app
+//
+//  Created by Pablo Philipps on 7/4/20.
+//
+
+import SwiftUI
+
+@main
+struct pablq_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
