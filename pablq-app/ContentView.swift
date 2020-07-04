c8dc715c82b45ed439dfed17297aca13b4423326
Pablo Philipps <pabloignaciophilipps@gmail.com>
2020-07-04 18:56:55 -0500

    Adds basic content view

--- pablq-app/ContentView.swift
+++ pablq-app/ContentView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedSport: Sport?
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            List {
+                ForEach(sports) { sport in
+                    HStack {
+                        Spacer()
+                        Image(sport.imageAssetName)
+                        Spacer()
+                    }.onTapGesture {
+                        selectedSport = sport
+                    }
+                }
+                VStack {
+                    Text("https://github.com/pablq/pablq-website")
+                }
+            }
+        }.sheet(item: $selectedSport) { sport in
+            GamesListView(sport: sport).onTapGesture {
+                selectedSport = nil
+            }
+        }
+    }
+    
+    private let sports: [Sport] = [
+        Sport(name: "mlb", imageAssetName: "baseball"),
+        Sport(name: "nhl", imageAssetName: "puck"),
+        Sport(name: "nfl", imageAssetName: "football"),
+        Sport(name: "nba", imageAssetName: "basketball")
+    ]
+}
+
+struct Sport: Identifiable {
+    var id = UUID()
+    let name: String
+    let imageAssetName: String
+}
+
+struct GamesListView: View {
+    let sport: Sport
+    
+    var body: some View {
+        Text(sport.name)
     }
 }
 
