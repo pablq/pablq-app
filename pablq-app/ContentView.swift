38ae48cf71420f94ec879fcdeb3ac9a6126acb9b
Pablo Philipps <pabloignaciophilipps@gmail.com>
2020-07-04 19:38:48 -0500

    Refines main screen interface.

--- pablq-app/ContentView.swift
+++ pablq-app/ContentView.swift
@@ -9,22 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     
+    private let sports: [Sport] = [
+        Sport(name: "mlb", imageAssetName: "baseball", activeImageAssetName: "baseball_active"),
+        Sport(name: "nhl", imageAssetName: "puck", activeImageAssetName: "puck_active"),
+        Sport(name: "nfl", imageAssetName: "football", activeImageAssetName: "football_active"),
+        Sport(name: "nba", imageAssetName: "basketball", activeImageAssetName: "basketball_active")
+    ]
+    
+    private let links: [Link] = [
+        Link(userFacingString: "https://github.com/pablq/pablq-website",
+             url: URL(string: "https://github.com/pablq/pablq-website")),
+        Link(userFacingString: "https://github.com/pablq/pablq-app",
+             url: nil),
+    ]
+    
     @State var selectedSport: Sport?
     
     var body: some View {
-        VStack {
+        VStack(alignment: .center) {
             List {
                 ForEach(sports) { sport in
                     HStack {
                         Spacer()
-                        Image(sport.imageAssetName)
+                        Image(
+                            selectedSport?.id == sport.id ? sport.activeImageAssetName : sport.imageAssetName)
                         Spacer()
-                    }.onTapGesture {
+                    }
+                    .padding(.all, 10)
+                    .onTapGesture {
                         selectedSport = sport
                     }
                 }
-                VStack {
-                    Text("https://github.com/pablq/pablq-website")
+                ForEach(links) { link in
+                    Button(action: {
+                        if let url = link.url {
+                            UIApplication.shared.open(url)
+                        }
+                    }, label: {
+                        Text(link.userFacingString).font(
+                            .footnote)
+                    })
                 }
             }
         }.sheet(item: $selectedSport) { sport in
@@ -33,19 +57,19 @@ struct ContentView: View {
             }
         }
     }
-    
-    private let sports: [Sport] = [
-        Sport(name: "mlb", imageAssetName: "baseball"),
-        Sport(name: "nhl", imageAssetName: "puck"),
-        Sport(name: "nfl", imageAssetName: "football"),
-        Sport(name: "nba", imageAssetName: "basketball")
-    ]
+ }
+
+struct Link: Identifiable {
+    let id = UUID()
+    let userFacingString: String
+    let url: URL?
 }
 
 struct Sport: Identifiable {
     var id = UUID()
     let name: String
     let imageAssetName: String
+    let activeImageAssetName: String
 }
 
 struct GamesListView: View {
