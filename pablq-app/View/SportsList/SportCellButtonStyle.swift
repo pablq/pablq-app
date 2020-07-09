d95972d822f4bc9826889de74ed9650abc29b050
Pablo Philipps <pabloignaciophilipps@gmail.com>
2020-07-08 21:40:04 -0500

    Adds basic animation to SportCellButtonStyle.

--- pablq-app/View/SportsList/SportCellButtonStyle.swift
+++ pablq-app/View/SportsList/SportCellButtonStyle.swift
@@ -13,9 +13,25 @@ struct SportCellButtonStyle: ButtonStyle {
             .foregroundColor(configuration.isPressed ? Color.red : Color.black)
             .background(Color.white)
             .cornerRadius(10.0)
-            .shadow(color: Color.black,
-                    radius: configuration.isPressed ? 10.0 : 25.0,
-                    x: 0,
-                    y: 15)
+            .shadow(
+                color: Color.black,
+                radius: 5.0,
+                x: 0,
+                y: configuration.isPressed ? 4.0 : 8.0
+            )
+            .transformEffect(
+                .init(translationX: 0,
+                      y: configuration.isPressed ? 0.0 : -4.0)
+            )
+            .animation(.default)
+    }
+}
+
+struct SportCellButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        Button(action: {}) {
+            Image("baseball")
+        }
+        .buttonStyle(SportCellButtonStyle())
     }
 }
